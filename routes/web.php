c60f2c5ec2b959c2f2efefa81dfdd61631b6c781
Alejandro Otalvaro <65875344+Alejandro2479@users.noreply.github.com>
2023-12-11 08:36:34 -0500

    update h
    update y

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/register', [RegisterController::class, 'create'])->name('register.index');
+Route::get('/login', [SessionController::class, 'create'])->name('login.index');
